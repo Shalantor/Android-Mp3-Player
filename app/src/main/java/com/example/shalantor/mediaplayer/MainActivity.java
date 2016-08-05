8c5b80f981d42c4bd5d126c03c2b2a03794702f0
shalantor <gkaraolanis@uth.gr>
2016-08-05 11:32:46 +0300

    Change some characteristics of image and deleted redundant images in project

--- app/src/main/java/com/example/shalantor/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/shalantor/mediaplayer/MainActivity.java
@@ -18,7 +18,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.GestureDetector;
